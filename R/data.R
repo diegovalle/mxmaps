3bf822a91de4f87da390a76c1bd6d8eb5ba1e270
Diego Valle-Jones <diegovalle@gmail.com>
2016-01-12 20:14:26 -0600

    state and municipio data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' A data.frame containing population estimates for Mexican states in 2015.
+#'
+#' @name df_mxstate
+#' @docType data
+#' @references Taken from the Encuesta Intercensal.
+#'
+#' @keywords data
+#' @usage data(df_mxstate)
+NULL
+
+#' A data.frame containing population estimates for Mexican municipios in 2015.
+#'
+#' @name df_mxmunicipio
+#' @docType data
+#' @references Taken from the Encuesta Intercensal..
+#'
+#' @keywords data
+#' @usage data(df_mxmunicipio)
+NULL
+
