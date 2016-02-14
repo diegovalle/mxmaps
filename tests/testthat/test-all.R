096f1edac8ed965b52e75a0740e3a1732c653c24
Diego Valle-Jones <diegovalle@gmail.com>
2016-02-13 20:23:34 -0600

    test string convertion

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -33,6 +33,20 @@ test_that("hexbin matches expectations",{
   expect_identical(p$labels$x, "long")
 })
 
+test_that("convert state codes to INEGI format",{
+  expect_identical(str_mxstate(c("01", "02")), c("01", "02"))
+  expect_identical(str_mxstate(c("12", "2")), c("12", "02"))
+  expect_identical(str_mxstate(c(32, 5)), c("32", "05"))
+  expect_warning(str_mxstate(c(92, 5)), "Invalid state codes detected")
+})
+
+test_that("convert municipio codes to INEGI format",{
+  expect_identical(str_mxmunicipio(c("01006", "02003")), c("01006", "02003"))
+  expect_identical(str_mxmunicipio(c("12", "2"), c("09", "4")), c("12009", "02004"))
+  expect_identical(str_mxmunicipio(c(32, 5), c(9, 18)), c("32009", "05018"))
+  expect_warning(str_mxmunicipio(c(32, 5), c(79, 18)), "Invalid codes detected")
+  expect_warning(str_mxmunicipio(c("01096", "02003")), "Invalid codes detected")
+})
 
 # test_that("INEGI choropleths match expectations",{
 #   # Insert your INEGI token here:
