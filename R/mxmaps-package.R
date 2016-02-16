14d98388fde039c574efe94c972ecc28f77128d0
Diego Valle-Jones <diegovalle@gmail.com>
2016-02-15 19:46:14 -0600

    import choroplethr_animate

---
+++ R/mxmaps-package.R
@@ -0,0 +1,10 @@
+#' Animate a list of chropleths
+#'
+#' @importFrom choroplethr choroplethr_animate
+#' @param choropleths A list of choropleths represented as ggplot2 objects.
+#' @return  Nothing.  However, a variable number of files are written to the current working directory.
+#'
+#' @export
+#' @rdname choroplethr_animate
+#' @name choroplethr_animate
+NULL
