5d9000a4e4ceff0308cda288254a871202bbb1c0
Diego Valle-Jones <diegovalle@gmail.com>
2016-02-25 19:27:32 -0600

    lat lng

--- R/leaflet.R
+++ R/leaflet.R
@@ -93,7 +93,7 @@ draw_mxleaflet <- function(topoJSON, lat, lng, mapzoom) {
 mxmunicipio_leaflet <- function(df, pal,
                                 fillColor, popup = "",
                                 weight = .2, color = "#555555", opacity = 1, fillOpacity = .8,
-                                lat = 23.8, lng = -102, mapzoom = 5, zoom = NULL) {
+                                lng = -102, lat = 23.8, mapzoom = 5, zoom = NULL) {
   #if (!requireNamespace("mxmapsData", quietly = TRUE)) {
   #  stop("Package mxmapsData is needed for this function to work. Please install it.", call. = FALSE)
   #}
