8ac3b16bc6d525082c15c0b9e515f5cb5597cafa
Diego Valle-Jones <git@diegovalle.net>
2026-07-02 18:59:18 -0600

    Pass lintr and stylr checks

--- tests/testthat/test-metro.R
+++ tests/testthat/test-metro.R
@@ -4,13 +4,13 @@ context("Metro Areas")
 # Source: Zonas metropolitanas por tamano poblacional
 # http://www.conapo.gob.mx/en/CONAPO/Zonas_metropolitanas_2010
 test_that("The number of metro areas is correct", {
-  expect_equal(length(na.omit(unique(df_mxmunicipio$metro_area))), 59)
+  expect_length(na.omit(unique(df_mxmunicipio$metro_area)), 59)
 })
 
 # No NAs in lat and long
 test_that("No NA values in latitude and longitude columns", {
-  expect_true(all(!is.na(df_mxmunicipio_2020$lat)))
-  expect_true(all(!is.na(df_mxmunicipio_2020$long)))
+  expect_false(anyNA(df_mxmunicipio_2020$lat))
+  expect_false(anyNA(df_mxmunicipio_2020$long))
 })
 
 context("Test that metro area codes were transcribed correctly")
@@ -19,21 +19,39 @@ context("Test that metro area codes were transcribed correctly")
 # http://www.conapo.gob.mx/en/CONAPO/Zonas_metropolitanas_2010
 test_that("The number of municipios in metro areas is correct", {
   # Total municipios in all metro areas
-  expect_equal(length(unique(df_mxmunicipio_2020$region[!is.na(df_mxmunicipio_2020$metro_area)])), 367)
+  expect_length(unique(df_mxmunicipio_2020$region[
+    !is.na(df_mxmunicipio_2020$metro_area)
+  ]), 367)
   # Total municipios by metro area
-  df <- aggregate(df_mxmunicipio_2020, by=list(metro = df_mxmunicipio_2020$metro_area), function(x) length(x))
-  expect_equal(df[df$metro == "Monterrey",]$metro_area,
-               13)
-  expect_equal(df[df$metro == "Puerto Vallarta",]$metro_area,
-               2)
-  expect_equal(df[df$metro == "Cuernavaca",]$metro_area,
-               8)
-  expect_equal(df[df$metro == "Villahermosa",]$metro_area,
-               2)
-  expect_equal(df[df$metro == "Orizaba",]$metro_area,
-               12)
-  expect_equal(df[df$metro == "Tianguistenco",]$metro_area,
-               6)
-  expect_equal(df[df$metro == "Tula",]$metro_area,
-               5)
+  df <- aggregate(df_mxmunicipio_2020,
+    by = list(metro = df_mxmunicipio_2020$metro_area), function(x) length(x)
+  )
+  expect_identical(
+    df[df$metro == "Monterrey", ]$metro_area,
+    13L
+  )
+  expect_identical(
+    df[df$metro == "Puerto Vallarta", ]$metro_area,
+    2L
+  )
+  expect_identical(
+    df[df$metro == "Cuernavaca", ]$metro_area,
+    8L
+  )
+  expect_identical(
+    df[df$metro == "Villahermosa", ]$metro_area,
+    2L
+  )
+  expect_identical(
+    df[df$metro == "Orizaba", ]$metro_area,
+    12L
+  )
+  expect_identical(
+    df[df$metro == "Tianguistenco", ]$metro_area,
+    6L
+  )
+  expect_identical(
+    df[df$metro == "Tula", ]$metro_area,
+    5L
+  )
 })
